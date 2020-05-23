aa87548231ea68ff8519ae83cd117c639642f20e
DevFGStudio <47854642+DevFGStudio@users.noreply.github.com>
2020-05-24 07:08:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = process.argv.length == 2 ? process.env.token : "";
+const token = 'NzEzODU1OTMzNjIwNDIwNjQ5.XsmeoA.8u_T51E9VXvHAwhMaUX0_DTXGDA';
 const moment = require("moment");
 require("moment-duration-format");
 const welcomeChannelName = "안녕하세요";
